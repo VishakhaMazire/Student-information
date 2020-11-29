4193d8f626e9293fef1dfb2b4ee287f6a7f40aa8
VishakhaMazire <mazirevishakha26@gmail.com>
2020-11-29 14:29:56 +0530

    Added: Displaying student id and name

--- student.java
+++ student.java
@@ -1,8 +1,22 @@
 class student
 {
-        public static void main(String args[])
+        String name;
+        int id;
+        student(String n, int i)
         {
-                System.out.println("Welcome To student information display");
+        name=n;
+        id=i;
         }
 
+ void display()
+{
+        System.out.println(id+" "+name);
+}
+        public static void main(String args[])
+{
+student s1=new student("Vishakha", 1);
+student s2=new student("Sailee", 2);
+s1.display();
+s2.display();
+}
 }
